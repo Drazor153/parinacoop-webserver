fbf4cc5c5531b8174c03987acaccbb6397546110
Drazor153 <fabianjusto2341@gmail.com>
2024-10-23 12:53:32 -0300

    fix: migrations reset

--- src/database/migrator.ts
+++ src/database/migrator.ts
@@ -1,5 +1,26 @@
-function makeMigration(){
-    
+import { writeFile } from 'fs/promises';
+import { join } from 'path';
+async function makeMigration() {
+  const args = process.argv.slice(2);
+
+  const migrationsPath = './migrations';
+  const migrationName = args[0].replace(' ', '_');
+  const now = Date.now();
+
+  await writeFile(
+    join(__dirname, migrationsPath, `${now}_${migrationName}.ts`),
+`import { Kysely, sql } from 'kysely';
+
+export async function up(db: Kysely<any>): Promise<void> {
+  
 }
 
-makeMigration()
+export async function down(db: Kysely<any>): Promise<void> {
+  
+}
+
+`,
+  );
+}
+
+makeMigration();
